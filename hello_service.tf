e187a628cfee249fad2651967c7d9a031e770316
German Moya <pbacterio@gmail.com>
2018-09-17 12:48:04 +0200

    ConfigMap used as volume and environment vars

--- hello_service.tf
+++ hello_service.tf
@@ -2,14 +2,42 @@ resource "kubernetes_replication_controller" "hello-app" {
   metadata {
     name = "hello-app"
   }
+
   spec {
     selector {
       app = "hello-app"
     }
+
+    replicas = 2
+
     template {
       container {
         image = "pbacterio/demoapp"
-        name  = "helloapp"
+        name  = "hello-app"
+
+        env {
+          name = "NAME"
+
+          value_from {
+            config_map_key_ref = {
+              name = "hello-app"
+              key  = "name"
+            }
+          }
+        }
+
+        volume_mount {
+          name       = "hello-app"
+          mount_path = "/conf"
+        }
+      }
+
+      volume {
+        name = "hello-app"
+
+        config_map {
+          name = "hello-app"
+        }
       }
     }
   }
@@ -19,13 +47,25 @@ resource "kubernetes_service" "hello-app" {
   metadata {
     name = "hello-app"
   }
+
   spec {
     selector {
       app = "hello-app"
     }
+
     port {
-      port = 8080
+      port        = 8080
       target_port = 8080
     }
+  }
+}
+
+resource "kubernetes_config_map" "hello-app" {
+  metadata {
+    name = "hello-app"
+  }
+
+  data {
+    name = "KLAATU BARADA NIKTO"
   }
 }
